b2389f6b06e5d144025a4e2cd1f9c834d6142924
Max Nedr <maxim.nedozrelov@ya.ru>
2019-01-24 10:48:13 +0300

    второй вариант

--- yii2-app-basic/controllers/TestController.php
+++ yii2-app-basic/controllers/TestController.php
@@ -81,7 +81,7 @@ class TestController extends Controller
         $query = new Query();
         $newModel = $query->from('user')->where(['id' => 1])->one();
         $query_2 = new Query();
-        $newModel_2 = $query_2->from('user')->where('id > 1')->orderBy('username')->all();
+        $newModel_2 = $query_2->from('user')->where(['>','id','1'])->orderBy('username')->all();
         $query_3 = new Query();
         $newModel_3 = $query_3->from('user')->count();
         $query_4 = new Query();
